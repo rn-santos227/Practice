d94a3408834bf32bf1a31276d90d055978a4161d
janarielsanjose <janarielsanjose@gmail.com>
2017-06-27 17:23:33 +0800

    Fixed a glitch

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Practice</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css"/>>
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
         <link rel="stylesheet" href="css/stylesheet.css"/>
     </head>
     
